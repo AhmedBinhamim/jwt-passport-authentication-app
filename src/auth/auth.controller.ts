ab48fd284db7e28cccd9dac323dc7477ea583e94
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-07-22 21:00:33 +0300

    implemented local guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
-import { Body, Controller, HttpException, Post } from '@nestjs/common';
+import { Body, Controller, HttpException, Post, UseGuards } from '@nestjs/common';
 import { AuthPayloadDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalGuard } from './guards/local.guard';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService){}
     @Post('login')
+    @UseGuards(LocalGuard)
     login(@Body() authPayLoad: AuthPayloadDto){
         const user = this.authService.validateUser(authPayLoad);
         if(!user) throw new HttpException('Invalid Credintials', 401);
