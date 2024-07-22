7116212ac5860729d19b8f22509ceea57260de9c
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-07-22 20:38:32 +0300

    implemented login endpoint to get JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, Post } from '@nestjs/common';
+import { AuthPayloadDto } from './dto/auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService){}
+    @Post('login')
+    login(@Body() authPayLoad: AuthPayloadDto){
+        const user = this.authService.validateUser(authPayLoad);
+        if(!user) throw new HttpException('Invalid Credintials', 401);
+        return user;
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { AuthPayloadDto } from './dto/auth.dto';
+import { find } from 'rxjs';
+import { JwtService } from '@nestjs/jwt';
+
+const fakeUsers = [
+    {
+        id: 1,
+        username: 'ahmed',
+        password: 'password'
+    },
+
+    {
+        id: 2,
+        username: 'mohammed',
+        password: 'password123'
+    }
+]
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private jwtService: JwtService){
+
+    }
+    validateUser({username, password}: AuthPayloadDto){
+        const findUser = fakeUsers.find((user) => user.username === username);
+        if(!findUser) return null;
+        if(password === findUser.password){ 
+            const {password, ...user} = findUser;
+            return this.jwtService.sign(user);    
+        }
+    }
+}
