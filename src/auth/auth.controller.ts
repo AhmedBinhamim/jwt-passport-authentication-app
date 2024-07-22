942680583ed7d5ca24494b2f56b3ec9cd29b36f9
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-07-22 21:06:02 +0300

    added express request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,23 @@
-import { Body, Controller, HttpException, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpException, Post,  UseGuards } from '@nestjs/common';
 import { AuthPayloadDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalGuard } from './guards/local.guard';
+import { request } from 'express';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService){}
+
     @Post('login')
     @UseGuards(LocalGuard)
     login(@Body() authPayLoad: AuthPayloadDto){
         const user = this.authService.validateUser(authPayLoad);
         if(!user) throw new HttpException('Invalid Credintials', 401);
         return user;
+    }
+
+    @Get('status'){
+        status(@Req() req: request)
     }
 }
